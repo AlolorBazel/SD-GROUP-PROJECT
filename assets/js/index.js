6a0c4dd5e782128b800a5c6b92c842cca280c729
AlolorBazel <alolor.bazel@gmail.com>
2024-07-26 22:27:17 +0800

    First Commit

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+
+function openPaymentForm() {
+    window.open("payment-form.html", "_blank");
+    window.close();
+}
+
+function openInformationForm(){
+    window.open("index.html","_blank");
+    window.close();
+}
+
+function openCommentForm(){
+    window.open("comment-form.html","_blank");
+    window.close();
+}
